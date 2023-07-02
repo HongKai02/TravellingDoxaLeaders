1cd9d9b807e4684e1994151d6dda0b2d8125e7d6
hongkai0203 <hongkai0203@gmail.com>
2023-07-02 16:06:46 -0500

    Sync with backend to get data of RSVPed riders

--- tdl_app/src/components/RidersList.js
+++ tdl_app/src/components/RidersList.js
@@ -1,14 +1,15 @@
 import {useEffect, useState} from 'react'
+import {PiInfoThin} from "react-icons/pi"
 
 function RidersList(){
     const [riders, setRiders] = useState();
 
     useEffect(() => {
         console.log('Fettching...')
-        fetch('http://127.0.0.1:8000/api/rider/')
+        fetch('http://127.0.0.1:8000/api/rsvpedriders/')
         .then((response) => response.json())
         .then((data) => {
-            setRiders(data.riders);
+            setRiders(data.RSVPedRiders);
         })
     }, []) // The empty list here is to specify that this effect should only run once, on load
 
@@ -17,15 +18,17 @@ function RidersList(){
             <div className="list-of-riders">
                 <ul>
                     {riders ? riders.map((rider) => {
-                        return <li>{rider.firstName} {rider.lastName} <span className="rider-list-info-icon"> i </span></li>;
+                        console.log(rider)
+                        return <li>{rider.riderID.firstName} {rider.riderID.lastName} <span className="rider-list-info-icon"><PiInfoThin /></span></li>;
                     }) : null}
                 </ul>
             </div>
             <div>
                 <a href="Google.com">Edit List</a>
-                <p>A total of {riders.length} people signed up, and at least {Math.ceil(riders.length/4)} cars will be needed</p>
+                <p>A total of {riders && riders.length} people signed up, and at least {riders && Math.ceil(riders.length/4)} cars will be needed</p>
                 <button className="submit-button">Log In</button>
             </div>
+            
             
         </div>
     )
