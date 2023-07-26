64bf11010a38a1529c47a14cccc29d4844b8db87
hongkai0203 <hongkai0203@gmail.com>
2023-07-26 17:24:52 -0500

    Successful API calls for multiple destinations

--- tdl_backend/populateCoordinates.py
+++ tdl_backend/populateCoordinates.py
@@ -11,7 +11,8 @@ from itertools import chain
 
 
 
-
+def get_address(rider):
+    return rider.addressLine1 + (' ' if rider.addressLine2 else '' ) + rider.addressLine2 + ' ' + rider.city + ' ' + rider.zipcode + ' ' + rider.state
 
 
 # Get list of all riders
@@ -21,55 +22,41 @@ second_rider = Rider.objects.get(riderID=2)
 
 # Get event details
 event = Event.objects.get(eventID = 2) # This should be changed to get this Friday's 
-print(event.addressLine1, event.addressLine2, event.city, event.zipcode, event.state)
+event_address = get_address(event)
 
 all_riders = all_riders[: 3] # This statement is only for development purposes (to reduce api calls)
 
+# API details to Google Distance Matrix API
+api_key = "AIzaSyAk4Bfap0nzIjFV_M8kLqF9KP3r6Y1AanM"
+url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
 for i in range(len(all_riders)):
     source = all_riders[i]
     dest = list(chain(all_riders[ : i], all_riders[i+1 : ]))
-    #print("##################" + str(source) + "(Source) ##################")
-    #for j in range(len(dest)):
-        #print(dest[j])
+
     formatted_dest = ''
     for j in range(len(dest)):
-        formatted_dest = formatted_dest + '|' + dest[j].addressLine1 + ' ' + dest[j].addressLine2 + ' ' + dest[j].city + ' ' + dest[j].zipcode + ' ' + dest[j].state
+        formatted_dest = formatted_dest + '|' + get_address(dest[j])
+    formatted_dest = formatted_dest[1: ] + '|' + event_address
+    print(event_address)
     print(formatted_dest)
-
+    print('\n')
+    r = requests.get(
+        url + "origins=" + get_address(source) + "&destinations=" + formatted_dest + "&key=" + api_key
+    )
+    x = r.json()
+    print(x)
+    with open("travelDistance2.txt", "a") as f:
+        f.write(str(x))
     
 
-
-    
-
-
-
-# API call to Google Distance Matrix API
-api_key = "AIzaSyAk4Bfap0nzIjFV_M8kLqF9KP3r6Y1AanM"
-url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
-
-source = "409 W Johnson St, Madison Wisconsin"
-
-dest = "7012 Companion Lane, Middleton Wisconsin"
-
 '''
-r = requests.get(
-    url + "origins=" + source + "&destinations=" + dest + "&key=" + api_key
-)
-
-x = r.json()
-
-print(x)
-
-with open("travelDistance.txt", "w") as f:
-    f.write(str(x))
-'''
-
-
 trip = RiderToRiderTravelDistance()
 trip.fromRider = first_rider
 trip.toRider = second_rider
 trip.drivingDistance = 0.1
 trip.drivingTime = 1
 trip.save()
+'''
+
 
