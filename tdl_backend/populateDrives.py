1b215679f33c5fb117eb9c964deb69e2ff272808
hongkai0203 <hongkai0203@gmail.com>
2023-08-19 21:10:15 -0500

    Handles the fact that destination is now a rider

--- tdl_backend/populateDrives.py
+++ tdl_backend/populateDrives.py
@@ -21,14 +21,20 @@ def set_address_changed():
 # Get list of all riders
 all_riders = Rider.objects.all()
 
+# Set all rider's addressChanged to false
+for rider in all_riders:
+    rider.addressChanged = True
+
+Rider.objects.bulk_update(all_riders, ['addressChanged'])
+
 # Get event details
 event = Event.objects.get(eventID = 2) #TODO: This should be changed to get this Friday's 
 event_address = get_address(event)
 
-all_riders = all_riders[: 3] # This statement is only for development purposes (to reduce api calls)
+#all_riders = all_riders[:3] # This statement is only for development purposes (to reduce api calls)
 
 # API details to Google Distance Matrix API
-#api_key = "AIzaSyAk4Bfap0nzIjFV_M8kLqF9KP3r6Y1AanM"
+api_key = "AIzaSyAk4Bfap0nzIjFV_M8kLqF9KP3r6Y1AanM"
 url = "https://maps.googleapis.com/maps/api/distancematrix/json?"
 
 for i in range(len(all_riders)):
@@ -40,41 +46,61 @@ for i in range(len(all_riders)):
         if source.addressChanged or dest[j].addressChanged:
             formatted_dest = formatted_dest + '|' + get_address(dest[j])
 
-    formatted_dest = formatted_dest[1: ] + '|' + event_address # Trim off first deliminator and add event address
+    #formatted_dest = formatted_dest[1: ] + '|' + event_address # Trim off first deliminator and add event address
+
+    formatted_dest = formatted_dest[1: ]
+
+    #print(formatted_dest)
 
     # API Call (Commented out for testing purposes)
-    #r = requests.get(
-     #   url + "origins=" + get_address(source) + "&destinations=" + formatted_dest + "&key=" + api_key
-    #)
+    r = requests.get(
+        url + "origins=" + get_address(source) + "&destinations=" + formatted_dest + "&key=" + api_key
+    )
+
+    r = r.json()
+
+    #print(r)
 
     ##################################### Development code to reduce api calls ###############################
-
+    '''
     x = {}
     filepath = "jsonPlayGround/file" + str(i) + ".json"
     with open(filepath, 'r') as f:
         x = json.load(f)
-
+    '''
     ###########################################################################################################
 
-    all_trip_info = x['rows'][0]['elements'] #TODO: Replace x with r
+    all_trip_info = r['rows'][0]['elements'] #TODO: Replace x with r
+    #print(all_trip_info)
 
+    print("From rider is")
+    print(source)
     # Enter data into 'rider to rider travel distance' table
     for count, toRider in enumerate(dest):
-
+        print(toRider)
         # Get driving distance and driving time
         drivingDistance = all_trip_info[count]['distance']['text']
         drivingTime = all_trip_info[count]['duration']['text']
 
         # Removing trailing strings
-        formatted_driving_distance = drivingDistance[: len(drivingDistance) - 3]
-        formatted_driving_time = drivingTime[: len(drivingTime) - 5]
+        print(drivingDistance)
+        if drivingDistance[len(drivingDistance) -2: ] == "km":
+            formatted_driving_distance = drivingDistance[: len(drivingDistance) - 3]
+        
+        else: formatted_driving_distance = drivingDistance[:len(drivingDistance)- 2]
+
+        print(drivingTime)
+        if drivingTime[len(drivingTime) - 4: ] == "mins":
+            formatted_driving_time = drivingTime[:len(drivingTime) -5]
+
+        else: formatted_driving_time = drivingTime[: len(drivingTime) - 4]
 
         # Preparing trip database entry
         trip = RiderToRiderTravelDistance()
         trip.fromRider = source
         trip.toRider = toRider
-        trip.drivingTime = formatted_driving_time
-        trip.drivingDistance = formatted_driving_distance
+        trip.drivingTime = float(formatted_driving_time)
+        trip.drivingDistance = float(formatted_driving_distance)
 
         trip.save()
 
